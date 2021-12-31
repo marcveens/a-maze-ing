52e645c4060f706c1a55d7cb69417b6edde66e23
Marc Veens <marcveens@hotmail.com>
2021-12-31 15:03:44 +0100

    Deploy test

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://marcveens.github.io/a-maze-ing/',
+  base: '/a-maze-ing/',
   plugins: [react()]
 })
